11f660ddc80093cc3bf29725169cdbb0dca1eb34
Facu <subiabrewd@gmail.com>
2020-07-23 23:50:57 +0200

    Add NotBlank constraint to category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -20,6 +21,7 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $name;
 
