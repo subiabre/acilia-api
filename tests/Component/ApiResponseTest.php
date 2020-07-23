0b993f9579f0fbcb14b227cc9c65abf0b8d80109
Facu <subiabrewd@gmail.com>
2020-07-24 00:16:12 +0200

    Make ApiResponse tests

---
+++ tests/Component/ApiResponseTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Component;
+
+use App\Component\ApiResponse;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiResponseTest extends TestCase
+{
+    public function testErrorReturnsExpectedResponse()
+    {
+        $response = new ApiResponse();
+        $data = ['key' => 'value'];
+
+        $expected = new JsonResponse(['error' => $data], 400);
+        $actual = $response->error($data);
+        
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertInstanceOf(JsonResponse::class, $actual);
+
+        $this->assertSame($expected->getContent(), $actual->getContent());
+        $this->assertSame($expected->getStatusCode(), $actual->getStatusCode());
+    }
+
+    public function testDataReturnsExpectedResponse()
+    {
+        $response = new ApiResponse();
+        $data = ['key' => 'value'];
+
+        $expected = new JsonResponse(['data' => $data], 200);
+        $actual = $response->data($data);
+        
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertInstanceOf(JsonResponse::class, $actual);
+
+        $this->assertSame($expected->getContent(), $actual->getContent());
+        $this->assertSame($expected->getStatusCode(), $actual->getStatusCode());
+    }
+}
