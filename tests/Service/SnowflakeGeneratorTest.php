81cf161e01e66e640afce3612a8e892fcbbc885b
Facu <subiabrewd@gmail.com>
2020-07-23 21:16:05 +0200

    Make snowflake generator tests

---
+++ tests/Service/SnowflakeGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\SnowflakeGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SnowflakeGeneratorTest extends KernelTestCase
+{
+    /**
+     * @var SnowflakeGenerator
+     */
+    private $snowflake;
+
+    public function setUp(): void{
+        self::bootKernel();
+        $this->snowflake = self::$kernel->getContainer()->get('app.service.snowflake-generator');
+    }
+
+    public function testExplicitlyGeneratesId()
+    {
+        $id1 = $this->snowflake->getId();
+        $id2 = $this->snowflake->getId();
+        $id3 = $this->snowflake->new();
+
+        $this->assertSame($id1, $id2);
+        $this->assertNotSame($id1, $id3);
+    }
+
+    public function testInstanceToString()
+    {
+        $instance = $this->snowflake->getId();
+        $string = (string) $this->snowflake;
+
+        $this->assertSame($instance, $string);
+    }
+}
