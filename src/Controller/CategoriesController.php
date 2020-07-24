c10a50d6e7b3b6072c7b2bbe94fafbfb6fff6151
Facu <subiabrewd@gmail.com>
2020-07-24 03:45:39 +0200

    Use ValidationErrors

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -5,26 +5,13 @@ namespace App\Controller;
 use App\Component\ApiResponse;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Util\ValidationErrors;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-
-function listValidationErrors($errors)
-{
-    $errorList = [];
-
-    foreach ($errors as $error) {
-        $path = $error->getPropertyPath();
-        $message = $error->getMessage();
-
-        $errorList[$path] = $message;
-    }
-
-    return $errorList;
-}
 
 class CategoriesController extends AbstractController
 {
@@ -36,6 +23,7 @@ class CategoriesController extends AbstractController
         ApiResponse $response,
         EntityManagerInterface $em, 
         ValidatorInterface $validator,
+        ValidationErrors $validationErrors,
         NormalizerInterface $normalizer
     )
     {
@@ -49,7 +37,7 @@ class CategoriesController extends AbstractController
         $errors = $validator->validate($category);
 
         if (\count($errors) > 0) {
-            $errorList = listValidationErrors($errors);
+            $errorList = $validationErrors->list($errors);
 
             return $response->error('Malformed payload', $errorList);
         }
@@ -92,6 +80,7 @@ class CategoriesController extends AbstractController
         ApiResponse $response,
         EntityManagerInterface $em,
         ValidatorInterface $validator,
+        ValidationErrors $validationErrors,
         NormalizerInterface $normalizer,
         CategoryRepository $categories
     )
@@ -110,7 +99,7 @@ class CategoriesController extends AbstractController
         $errors = $validator->validate($category);
 
         if (\count($errors) > 0) {
-            $errorList = listValidationErrors($errors);
+            $errorList = $validationErrors->list($errors);
 
             return $response->error('Malformed payload', $errorList);
         }
